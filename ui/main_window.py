e4ddb21a1a34e685b086973fc87d7aa970c252ac
ethanposey <100727989+nov8r@users.noreply.github.com>
2025-05-27 22:13:50 -0400

    made some tiny modifications, started on the filters section

--- ui/main_window.py
+++ ui/main_window.py
@@ -12,25 +12,33 @@ class ImageEditorApp(tk.Tk):
         self.title("Image Editor")
         self.geometry("900x900")
         self.resizable(True, True)
+        self.configure(bg="#1f1f1f")
         self._create_menubar()
 
     def _create_menubar(self):
         menubar = tk.Menu(self)
         self.config(menu=menubar)
 
+        # File Menu
         file_menu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label="File", menu=file_menu)
-
         file_menu.add_command(label="Open image...", command=self._open_image)
         file_menu.add_command(label="Save image", command=self._save_image)
         file_menu.add_command(label="Save image as...", command=self._save_image_as)
         file_menu.add_separator()
         file_menu.add_command(label="Exit", command=self.quit)
 
+        # Edit Menu
         edit_menu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label="Edit", menu=edit_menu)
         edit_menu.add_command(label="Rotate", command=self._rotate_image)
         edit_menu.add_command(label="Resize", command=self._resize_image)
+
+        # Effect Menu - WIP
+        # effect_menu = tk.Menu(menubar, tearoff=0)
+        # menubar.add_cascade(label="Effects", menu=effect_menu)
+        # edit_menu.add_command(label="Rotate", command=self._rotate_image)
+        # edit_menu.add_command(label="Resize", command=self._resize_image)
 
     def _open_image(self):
         file_path = fd.askopenfilename(
@@ -44,7 +52,7 @@ class ImageEditorApp(tk.Tk):
             print(f"Selected file: {file_path}")
             self.img = open_img(file_path)
             self.photo = ImageTk.PhotoImage(self.img)
-            self.label = tk.Label(self, image=self.photo)
+            self.label = tk.Label(self, image=self.photo, bd=0)
             self.label.place(relx=0.5, rely=0.5, anchor="center")
 
     def _save_image(self):
